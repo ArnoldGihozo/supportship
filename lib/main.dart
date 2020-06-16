33e278292a8e2fe7d72e8810438f2deef9092fd4
arnoldgihozo <gihozo@ualberta.ca>
2020-06-15 19:39:19 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  // This widget is the root of your application.
+  
+}
+
