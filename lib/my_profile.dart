e710d033b9017dc8b14ca25e5bf632fc480a2adb
amreesalmonte <amreesalmonte@gmail.com>
2020-06-25 12:23:51 -0600

    intialized file

---
+++ lib/my_profile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void profile() {
+  runApp(MyProfile());
+}
+
+class MyProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+  // This widget is the root of your application.
+
+}
